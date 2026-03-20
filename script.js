559b6dde0d7becbd62ce9e1cd9b7982eed79532c
kadirshexbzeni <kadir.shexbzeni@gmail.com>
2026-03-21 00:01:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-اااااااجججaaaddffimport { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, push, onChildAdded, remove, onChildRemoved } 
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
